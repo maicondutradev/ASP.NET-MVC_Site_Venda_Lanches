8fa7f3f9cb961e8188eae7bf651a9ea8dcad3c2b
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-07-15 09:44:58 -0300

    update

---
+++ LanchesSaq/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using LanchesSaq.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace LanchesSaq.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
